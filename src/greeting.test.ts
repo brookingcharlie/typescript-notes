122f38b8a215162580deaf25ec7df9ed24fb3d11
Charlie Brooking <charlie@brooking.id.au>
2020-09-05 18:43:09 +1000

    add describe to jest test

--- src/greeting.test.ts
+++ src/greeting.test.ts
@@ -1,6 +1,8 @@
 import { Student, greeting} from './greeting';
 
-test('greets fred', () => {
-  const joe: Student = new Student('S1234567', 'Joe');
-  expect(greeting(joe)).toBe('Hello, Joe!')
+describe('greeting', () => {
+  test('greets fred', () => {
+    const joe: Student = new Student('S1234567', 'Joe');
+    expect(greeting(joe)).toBe('Hello, Joe!')
+  });
 });
