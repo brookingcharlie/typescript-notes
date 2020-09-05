ef01d6118faccf360bcd7df89979cb01321dc394
Charlie Brooking <charlie@brooking.id.au>
2020-09-05 22:43:46 +1000

    remove dummy greeting code

--- src/greeting.test.ts
+++
@@ -1,8 +0,0 @@
-import { Student, greeting} from './greeting';
-
-describe('greeting', () => {
-  test('greets fred', () => {
-    const joe: Student = new Student('S1234567', 'Joe');
-    expect(greeting(joe)).toBe('Hello, Joe!')
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { Student, greeting} from './greeting';
+interface Person {
+  name: string;
+}
+
+class Student {
+  constructor(public id: string, public name: string) {}
+}
+
+function greeting(person: Person) {
+  return `Hello, ${person.name}!`;
+}
 
 const joe: Student = new Student('S1234567', 'Joe');
 
